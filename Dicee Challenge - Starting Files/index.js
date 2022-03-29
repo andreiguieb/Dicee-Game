162872cf0e5514087ca935b680be4393a87ffbff
andreiguieb <58454762+andreiguieb@users.noreply.github.com>
2022-03-29 22:35:29 +0800

    Add files via upload

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,79 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6)+1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 =  "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);;
+
+if (randomNumber1 >  randomNumber2){
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins";
+}
+else if (randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 2 Wins🚩";
+} else{
+  document.querySelector("h1").innerHTML = "Draw!";
+}
+
+
+
+// if (randomNumber1 === 1){
+//     document.querySelector(".img1").setAttribute("src", "images/dice1.png");
+// }
+// else if (randomNumber1 === 2){
+//     document.querySelector(".img1").setAttribute("src", "images/dice2.png");
+// }
+// else if (randomNumber1 === 3){
+//     document.querySelector(".img1").setAttribute("src", "images/dice3.png");
+// }
+// else if (randomNumber1 === 4){
+//     document.querySelector(".img1").setAttribute("src", "images/dice4.png");
+// }
+// else if (randomNumber1 === 5){
+//     document.querySelector(".img1").setAttribute("src", "images/dice5.png");
+// }
+// else if (randomNumber1 === 6){
+//     document.querySelector(".img1").setAttribute("src", "images/dice6.png");
+// }
+//
+// var randomNumber2 = Math.floor(Math.random() * 7);
+//
+// if (randomNumber2 === 1){
+//     document.querySelector(".img2").setAttribute("src", "images/dice1.png");
+// }
+// else if (randomNumber2 === 2){
+//     document.querySelector(".img2").setAttribute("src", "images/dice2.png");
+// }
+// else if (randomNumber2 === 3){
+//     document.querySelector(".img2").setAttribute("src", "images/dice3.png");
+// }
+// else if (randomNumber2 === 4){
+//     document.querySelector(".img2").setAttribute("src", "images/dice4.png");
+// }
+// else if (randomNumber2 === 5){
+//     document.querySelector(".img2").setAttribute("src", "images/dice5.png");
+// }
+// else if (randomNumber2 === 6){
+//     document.querySelector(".img2").setAttribute("src", "images/dice6.png");
+// }
+//
+// var player1 = randomNumber1;
+// var player2 = randomNumber2;
+//
+// if (player1 > player2){
+//   document.querySelector("h1").innerHTML = "🚩Player 1 Wins";
+// }
+// else if (player1 === player2){
+//   document.querySelector("h1").innerHTML = "Draw!";
+// } else {
+//   document.querySelector("h1").innerHTML = "Player 2 Wins🚩";
+// }
